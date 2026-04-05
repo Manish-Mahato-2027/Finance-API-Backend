8fb2171076f56189d76d9de65c7ff23482d4f937
Manish-Kumar-Mahato <amit2004@gmail.com>
2026-04-05 16:32:37 +0530

    change setting

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,7 @@ USE_TZ = True
 import os
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
