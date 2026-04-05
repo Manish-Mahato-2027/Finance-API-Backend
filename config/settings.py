b4facf448803b13f4a164bd6599117a2cdaebc9a
Manish-Kumar-Mahato <amit2004@gmail.com>
2026-04-06 00:12:35 +0530

    change in setting

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['finance-backend.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    'https://finance-api-backend-jp1l.onrender.com',
+]
 
 
 # Application definition
